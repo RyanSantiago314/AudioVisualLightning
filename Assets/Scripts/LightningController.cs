2d9a157794a21a9e2faebd2faed4eb35b1e2d83d
Ryan Santiago <ryansantiago.d7s@gmail.com>
2019-05-23 00:04:48 -0700

    Added One Giant Bolt

--- Assets/Scripts/LightningController.cs
+++ Assets/Scripts/LightningController.cs
@@ -13,8 +13,13 @@ public class LightningController : MonoBehaviour
     public Material DupeBolt;
     public Material DupeSpark;
 
+    public Material SingleFlash;
+    public Material SingleBolt;
+    public Material SingleSpark;
+
     public ParticleSystem Lightning;
     public ParticleSystem LightningDupe;
+    public ParticleSystem BigBolt;
 
     // Start is called before the first frame update
     void Start()
@@ -62,6 +67,9 @@ public class LightningController : MonoBehaviour
         if (aveMag[4] > .8f)
             LightningDupe.Play();
 
+        if (aveMag[5] > .8f)
+            BigBolt.Play();
+
         Flash.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], .5f));
         SkyFlash.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], .5f));
         Bolt.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], 1.0f));
@@ -71,11 +79,18 @@ public class LightningController : MonoBehaviour
         DupeBolt.SetColor("_EmissiveColor", new Color(aveMag[2], aveMag[0], aveMag[1], 1.0f));
         DupeSpark.SetColor("_EmissiveColor", new Color(aveMag[2], aveMag[0], aveMag[1], 1.0f));
 
+        SingleFlash.SetColor("_EmissiveColor", new Color(aveMag[0], aveMag[1], aveMag[2], .5f));
+        SingleBolt.SetColor("_EmissiveColor", new Color(aveMag[0], aveMag[1], aveMag[2], 1.0f));
+        SingleSpark.SetColor("_EmissiveColor", new Color(aveMag[0], aveMag[1], aveMag[2], 1.0f));
+
         Bolt.SetFloat("_Emissiveness", aveMag[3]*.35f);
         SkyFlash.SetFloat("_Emissiveness", aveMag[3]*.07f);
 
         DupeBolt.SetFloat("_Emissiveness", aveMag[3] * .4f);
         DupeSpark.SetFloat("_Emissiveness", aveMag[3] * .5f);
+
+        SingleBolt.SetFloat("_Emissiveness", aveMag[3] * .4f);
+        SingleSpark.SetFloat("_Emissiveness", aveMag[3] * .5f);
 
     }
 }
