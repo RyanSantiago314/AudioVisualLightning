2e04f82bc8277215527caf14dd9d783ec2aae340
Ryan Santiago <ryansantiago.d7s@gmail.com>
2019-05-22 20:01:53 -0700

    git add

---
+++ Assets/Scripts/LightningController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningController : MonoBehaviour
+{
+    public Material Flash;
+    public Material SkyFlash;
+    public Material Bolt;
+    public Material Spark;
+
+    public Material DupeFlash;
+    public Material DupeBolt;
+    public Material DupeSpark;
+
+    public ParticleSystem Lightning;
+    public ParticleSystem LightningDupe;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int numPartitions = 8;
+        float[] aveMag = new float[numPartitions];
+        float partitionIndx = 0;
+        int numDisplayedBins = 512 / 2; //NOTE: we only display half the spectral data because the max displayable frequency is Nyquist (at half the num of bins)
+
+        for (int i = 0; i < numDisplayedBins; i++)
+        {
+            if (i < numDisplayedBins * (partitionIndx + 1) / numPartitions)
+            {
+                aveMag[(int)partitionIndx] += AudioPeer.spectrumData[i] / (512 / numPartitions);
+            }
+            else
+            {
+                partitionIndx++;
+                i--;
+            }
+        }
+
+        // scale and bound the average magnitude.
+        for (int i = 0; i < numPartitions; i++)
+        {
+            aveMag[i] = (float)0.5 + aveMag[i] * 100;
+            if (aveMag[i] > 100)
+            {
+                aveMag[i] = 100;
+            }
+        }
+
+        var em = Lightning.emission;
+        em.rateOverTime = 15 * aveMag[0];
+
+        var main = Lightning.main;
+        main.startSpeed = 90 * aveMag[1];
+
+        if (aveMag[4] > .8f)
+            LightningDupe.Play();
+
+        Flash.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], .5f));
+        SkyFlash.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], .5f));
+        Bolt.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], 1.0f));
+        Spark.SetColor("_EmissiveColor", new Color(aveMag[1], aveMag[2], aveMag[0], 1.0f));
+
+        DupeFlash.SetColor("_EmissiveColor", new Color(aveMag[2], aveMag[0], aveMag[1], .5f));
+        DupeBolt.SetColor("_EmissiveColor", new Color(aveMag[2], aveMag[0], aveMag[1], 1.0f));
+        DupeSpark.SetColor("_EmissiveColor", new Color(aveMag[2], aveMag[0], aveMag[1], 1.0f));
+
+        Bolt.SetFloat("_Emissiveness", aveMag[3]*.35f);
+        SkyFlash.SetFloat("_Emissiveness", aveMag[3]*.07f);
+
+        DupeBolt.SetFloat("_Emissiveness", aveMag[3] * .4f);
+        DupeSpark.SetFloat("_Emissiveness", aveMag[3] * .5f);
+
+    }
+}
---
+++ Assets/Shader/Lightning.shader
@@ -0,0 +1,82 @@
+﻿
+//Adapted from Example 5.3 in The CG Tutorial by Fernando & Kilgard
+Shader "CM163/LightningParticles"
+{
+    Properties
+    {
+        _EmissiveColor("Emissive Color", Color) = (1, 1, 1, 1)
+        _Emissiveness("Emissiveness", Range(0,10)) = 0
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    
+    SubShader
+    {
+        Tags {"Queue" = "Transparent" "RenderType" = "Opaque" }
+        LOD 100
+
+        Blend One One
+        ZWrite off
+        Pass {
+            Tags { "LightMode" = "ForwardAdd" } //Important! In Unity, point lights are calculated in the the ForwardAdd pass
+            Blend One One //Turn on additive blending if you have more than one point light
+          
+            
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            
+           
+            uniform float4 _LightColor0; //From UnityCG
+    
+            uniform float4 _EmissiveColor;
+            uniform float _Emissiveness;
+            uniform float _Opacity;
+            sampler _MainTex;       
+          
+            struct appdata
+            {
+                    float4 vertex : POSITION;
+                    float3 normal : NORMAL;
+                    float2 uv: TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                    float4 vertex : SV_POSITION;
+                    float3 normal : NORMAL;       
+                    float3 vertexInWorldCoords : TEXCOORD1;
+                    float2 uv: TEXCOORD0;
+            };
+
+ 
+           v2f vert(appdata v)
+           { 
+                v2f o;
+                o.vertexInWorldCoords = mul(unity_ObjectToWorld, v.vertex); //Vertex position in WORLD coords
+                o.normal = v.normal; //Normal 
+                o.uv = v.uv;
+                o.vertex = UnityObjectToClipPos(v.vertex); 
+                
+              
+
+                return o;
+           }
+
+           fixed4 frag(v2f i) : SV_Target
+           {
+                
+                float4 texColor = tex2D(_MainTex, i.uv);
+                //FINAL COLOR OF FRAGMENT
+                return float4(_EmissiveColor * _Emissiveness)*texColor;
+ 
+            }
+            
+            ENDCG
+ 
+            
+        }
+            
+    }
+}
